4f1f7301b32b88d5026b0befe1d0281078cea334
Kevin Downey <redchin@gmail.com>
2014-09-18 12:13:06 -0700

    update url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.manigfeald/graph "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/hiredman/graph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
